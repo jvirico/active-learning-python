1ebc6be1d2377414f0331231e6a7f339d1c5ef93
jvirico <jvirico@gmail.com>
2021-09-04 14:44:17 +0200

    playing with strings

--- basic/6_arrays.py
+++ basic/6_arrays.py
@@ -56,3 +56,40 @@ def is_monotone_increasing(arr):
     return retorno
 
 print('Is %s monotone increasing?: %s' % (arr,is_monotone_increasing(arr)))
+
+
+'''
+    Max consecutive ones
+        Input: nums = [1,1,0,1,1,1]
+        Output: 3
+        Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+'''
+
+def findMaxConsecutiveOnes(nums):
+    # nums is a list List[int]
+    max = 0
+    count = 0
+
+    for i in range(nums):
+        if i == 1:
+            count+=1
+        else:
+            max = count if count>max else max
+            count = 0
+    max = count if count>max else max
+
+
+'''
+    Find numbers with even number of digits
+'''
+def findNumbers(nums):
+    count = 0
+
+    for i in nums:
+        ln = len(str(i))
+        if(ln%2)==0:
+            count+=1
+
+    return count
+
+print(findNumbers([555,901,482,1771]))
