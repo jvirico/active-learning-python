0c4117d51dd1fff7fc1d8d8b876b3e741f8e5a86
jvirico <jvirico@gmail.com>
2021-09-16 08:01:15 +0200

    new case

--- other/leetcode.py
+++ other/leetcode.py
@@ -44,3 +44,17 @@ def duplicateZeros(arr):
 
 
 print(duplicateZeros(list([0,0,0,0,0])))
+
+## Merge Sorted Array
+# Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+# Output: [1,2,2,3,5,6]
+# Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+# The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+
+
+def merge(nums1,m,nums2,n):
+    nums1[m:]=nums2
+
+    return sorted(nums1)
+
+print(merge([1,2,3,0,0,0],3,[2,5,6],3))
