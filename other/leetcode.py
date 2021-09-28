4d3d07ff3126d1747d334142271d918042660255
jvirico <jvirico@gmail.com>
2021-09-28 08:07:40 +0200

    more leecode

--- other/leetcode.py
+++ other/leetcode.py
@@ -270,3 +270,60 @@ print(solution6(A))
 A = [3,1,2,4,3]
 print(solution7(A))'''
 
+'''
+    Input: nums = [0,1,2,2,3,0,4,2], val = 2
+    Output: 5, nums = [0,1,4,0,3,_,_,_]
+    Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
+    Note that the five elements can be returned in any order.
+    It does not matter what you leave beyond the returned k (hence they are underscores).
+'''
+def removeElement(nums,val):
+    
+    j = 0
+    k = len(nums)
+    for i in range(len(nums)):
+        if (nums[i]!= val):
+            nums[j]=nums[i]
+            j+=1
+        else:
+            k-=1
+    return k
+
+
+print(removeElement([0,1,2,2,3,0,4,2],2))
+
+def removeDuplicates(nums):
+    
+    j=0
+    k = len(nums)
+    aux = 0
+    for i in range(len(nums)):
+        if(i==0):
+            aux = nums[i]
+            j+=1
+            #k-=1
+        else:
+            if(nums[i]==aux):
+                k-=1
+            else:
+                nums[j]=nums[i]
+                j+=1
+                aux = nums[i]
+    return k
+
+print(removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
+
+
+def checkIfExists(arr):
+    doubles = [x*2 for x in arr]
+    zeros = 0
+    for i in arr:
+        if i == 0: zeros+=1
+        if(i in doubles and i != 0):
+            return True
+        elif( i in doubles and i == 0 and zeros > 1):
+            return True
+    return False
+
+print(checkIfExists([-2,0,10,-19,4,6,-8]))
+
